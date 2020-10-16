3ba126440bdbaa7f584f7ed564dc5f5187516f5e
Artem Voikov <arvo@opinov8.com>
2020-10-16 16:09:30 +0300

    task-1: Create IRepository

---
+++ ConsoleApp3/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public interface IRepository<T>
+    {
+        public T Get(int id);
+        public bool Create(T t);
+    }
+}
