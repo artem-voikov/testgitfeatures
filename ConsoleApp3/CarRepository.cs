99f1d5e8bf5217dbe825119ee535d94fc0d9aa13
Artem Voikov <arvo@opinov8.com>
2020-10-16 16:16:30 +0300

    task-2: Create car and car repository

---
+++ ConsoleApp3/CarRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class CarRepository
+    {
+        public Car Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
